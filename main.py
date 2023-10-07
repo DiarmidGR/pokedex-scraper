0003fdb777850d09fbe077b392eb7a29140f8d0d
Diarmid Rendell <diarmid@hotmail.ca>
2023-10-07 07:07:46 -0600

    seems to fetch desired data properly. now to implement it in my web app...

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    input_url = 'https://pokemondb.net/pokedex/game/black-white'
+    #input_url = 'https://pokemondb.net/pokedex/game/black-white'
+    input_url = 'https://pokemondb.net/pokedex'
 
     response = requests.get(input_url)
 
@@ -14,11 +15,50 @@ def main():
         # Parse the HTML content with BeautifulSoup
         soup = BeautifulSoup(html_content, 'html.parser')
 
+        # Initialize an empty set to store unique links
+        unique_links = set()
+
+        # Find all anchor elements with href containing '/pokedex/game/'
+        game_links = soup.find_all('a', href=lambda href: href and '/game/' in href)
+
+        # Extract and add unique links to the set
+        for link in game_links:
+            href = link.get('href')
+            unique_links.add('https://pokemondb.net' + href)
+
+        collection = []
+        # Scrape each link and append data to main collection
+        for link in unique_links:
+            game = link
+            if len(link.split('/game/')) == 2:
+                game = link.split('/game/')[1]
+            data = {
+                "game": game,
+                "pokedex": scrape_game(link),
+            }
+            collection.append(data)
+        export_json(collection, "pokedex_data.json")
+
+
+def export_json(data_input, name_output):
+    with open(name_output, "w") as write:
+        json.dump(data_input, write)
+        print(f"Json file '{name_output}' has been created.")
+
+
+def scrape_game(input_url):
+    response = requests.get(input_url)
+
+    # Initialize a list to store the data
+    collection = []
+
+    if response.status_code == 200:
+        html_content = response.text
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+
         # Find all div elements with class "infocard"
         infocard_spans = soup.find_all('span', class_='infocard-lg-data')
-
-        # Initialize a list to store the data
-        collection = []
 
         # Iterate through the infocard_data_spans list and extract Name and Number
         for span in infocard_spans:
@@ -36,35 +76,7 @@ def main():
 
             # Append the data to the collection
             collection.append(data)
-
-        export_json(collection, "pokemon_red.json")
-
-        # Create a Pandas DataFrame from the list of data
-        df = pd.DataFrame(collection)
-
-        # Create an Excel writer object
-        excel_writer = pd.ExcelWriter('pokemon_data.xlsx', engine='openpyxl')
-
-        # Add the DataFrame to the Excel writer object
-        df.to_excel(excel_writer, index=False, sheet_name='Pokemon Data')
-
-        # Get the workbook and worksheet objects
-        workbook = excel_writer.book
-        worksheet = excel_writer.sheets['Pokemon Data']
-
-        # Save the Excel file
-        excel_writer.close()
-
-        print("Excel file 'pokemon_data.xlsx' has been created.")
-
-    else:
-        print("Failed to retrieve the webpage. Status code:", response.status_code)
-
-
-def export_json(data_input, name_output):
-    with open(name_output, "w") as write:
-        json.dump(data_input, write)
-        print(f"Json file '{name_output}' has been created.")
+    return collection
 
 
 main()
